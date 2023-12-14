7f28f520e4cd405552a67cb45e6adbffd94625d2
Anil Piparaiya <anilkumarpiparaiya@gmail.com>
2023-12-14 17:35:55 +0530

    Change Layout to new one

--- script.js
+++ script.js
@@ -1,56 +1,23 @@
-var buttons = document.getElementsByClassName("button");
-var display = document.getElementById("display");
-
-// display.textContent = 0;
-var operand1 = 0;
-var operand2 = null;
-var operator = null; 
-
-function isOperator(value) {
-    return value == "+" || value == "-" || value == "*" || value == "/";
-}
-
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function () {
-
-        var value = this.getAttribute('data-value');
-        var text = display.textContent.trim();
-
-        if (isOperator(value)) {
-            operator = value;
-            operand1 = parseFloat(text);
-            display.textContent = "";
-        } 
-        else if (value == "ac") {
-            display.textContent = "";
-        } 
-        else if (value == "sign") {
-            operand1 = parseFloat(text);
-            operand1 = -1 * operand1;
-            display.textContent = operand1;
-        } 
-        else if (value == ".") {
-            if (text.length && !text.includes('.')) {
-                display.textContent = text + '.';
-            }
-        } 
-        else if (value == "%") {
-            operand1 = parseFloat(text);
-            operand1 = operand1 / 100;
-            display.textContent = operand1
-        } 
-        else if (value == "=") {
-            operand2 = parseFloat(text);
-            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
-            if (result) 
-            {
-                display.textContent = result;
-                operand1 = result;
-                operand2 = null;
-                operator = null;
-            }
-        } else {
-            display.textContent += value;
-        }
-    });
-}
+let inputBox = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+let string = "";
+buttons.forEach((element) => {
+  element.addEventListener("click", (b) => {
+    if (b.target.innerText == "=") {
+      string = String(eval(string));
+      inputBox.value = string;
+    } else if (b.target.innerText == "AC") {
+      string = "";
+      inputBox.value = string;
+    } else if (b.target.innerText == "DEL") {
+      string = string.substring(0, string.length - 1);
+      inputBox.value = string;
+    } else if (b.target.id == "plusMinus") {
+      string = String(-eval(string));
+      inputBox.value = string;
+    } else {
+      string += b.target.innerText;
+      inputBox.value = string;
+    }
+  });
+});
